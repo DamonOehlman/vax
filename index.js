55ac518d5717c7909350b3e03cd255985630ee23
Damon Oehlman <damon.oehlman@gmail.com>
2015-02-25 20:00:21 +1100

    Passthrough joi schema object types

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
+var ValidationError = require('error/validation');
+var Joi = require('joi');
+var JOI_EXPORTS = [
+  'any',
+  'alternatives',
+  'array',
+  'bool',
+  'boolean',
+  'binary',
+  'date',
+  'func',
+  'number',
+  'object',
+  'string',
+  'ref'
+];
+
 /**
   # vax
 
@@ -9,7 +26,7 @@
   To be completed.
 
 **/
-module.exports = function(opts) {
+var vax = module.exports = function(opts) {
   var next = (opts || {}).next || function() {};
 
   // get the relavant schemas
@@ -17,8 +34,21 @@ module.exports = function(opts) {
   var params = (opts || {}).params;
 
   function handleRequest(req, res, opts, cb) {
+//     Joi.validate(opts.params, schema, function(err, data) {
+//       if (err) {
+//         return cb(ValidationError(err.details));
+//       }
+
+//       console.log('need to generate credentials');
+//     });
+
     return next(req, res, opts, cb);
   }
 
   return handleRequest;
 };
+
+// passthrough the various Joi functions
+JOI_EXPORTS.forEach(function(key) {
+  vax[key] = Joi[key];
+});
